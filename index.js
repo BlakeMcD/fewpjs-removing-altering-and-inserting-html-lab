f2b4935c31d407eddbd199507c4254818f3a172f
BlakeMcD <blakemcd@gmail.com>
2021-07-25 11:51:49 +1000

    Assignment Passed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+//create h1
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Blake is the champion";
+newHeader.className = 'victory';
+
+newHeader.appendChild('document.body');
+
